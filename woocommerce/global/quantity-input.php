1ef4d6d1e9d5f5b421ef42e6c12ec6655d22f6db
KoalaZCooL <fazafauzanadhiman@gmail.com>
2018-01-16 15:10:43 +1100

    added product single page custom QTY buttons

--- woocommerce/global/quantity-input.php
+++ woocommerce/global/quantity-input.php
@@ -43,13 +43,53 @@ if ( $max_value && $min_value === $max_value ) {
 					break;
 		endforeach;
 		
-		?><pre><?=print_r([$specname,$specvalue],1)?></pre><?php 
+		?><pre style="display:none;"><?=print_r([$specname,$specvalue],1)?></pre><?php 
 		
 	}?>
 	<div class="specification"><?=$specvalue?></div>
 	<div class="quantity">
 		<label class="screen-reader-text" for="<?php echo esc_attr( $input_id ); ?>"><?php esc_html_e( 'Quantity', 'woocommerce' ); ?></label>
-		<input type="number" id="<?php echo esc_attr( $input_id ); ?>" class="input-text qty text" step="<?php echo esc_attr( $step ); ?>" min="<?php echo esc_attr( $min_value ); ?>" max="<?php echo esc_attr( 0 < $max_value ? $max_value : '' ); ?>" name="<?php echo esc_attr( $input_name ); ?>" value="<?php echo esc_attr( $input_value ); ?>" title="<?php echo esc_attr_x( 'Qty', 'Product quantity input tooltip', 'woocommerce' ) ?>" size="4" pattern="<?php echo esc_attr( $pattern ); ?>" inputmode="<?php echo esc_attr( $inputmode ); ?>" />
+		<input type="number"
+					 id="<?php echo esc_attr( $input_id ); ?>"
+					 class="input-text qty text"
+					 step="<?php echo esc_attr( $step ); ?>"
+					 min="<?php echo esc_attr( $min_value ); ?>"
+					 max="<?php echo esc_attr( 0 < $max_value ? $max_value : '' ); ?>"
+					 name="<?php echo esc_attr( $input_name ); ?>"
+					 value="<?php echo esc_attr( $input_value ); ?>"
+					 title="<?php echo esc_attr_x( 'Qty', 'Product quantity input tooltip', 'woocommerce' ) ?>"
+					 size="4"
+					 pattern="<?php echo esc_attr( $pattern ); ?>"
+					 inputmode="<?php echo esc_attr( $inputmode ); ?>" />
 	</div>
-	<?php
+	<?php if(is_product()){?>
+	<div class="qty-nav">
+		<div class="qty-up"></div>
+		<div class="qty-nav-divider"></div>
+		<div class="qty-down"></div>
+	</div>
+	<script> 
+	jQuery(function() {
+		jQuery(".qty-up").on("click", function(event) {
+			event.stopPropagation();
+			var target = jQuery("#<?php echo esc_attr( $input_id ); ?>");
+			var oldValue = target.val();
+			var newVal = parseInt(oldValue) + 1;
+			target.val(newVal);
+		});
+		jQuery(".qty-down").on("click", function(event) {
+			event.stopPropagation();
+			var target = jQuery("#<?php echo esc_attr( $input_id ); ?>");
+			var oldValue = target.val();
+			// Don't allow decrementing below zero
+			if (oldValue > 0) {
+				var newVal = parseInt(oldValue) - 1;
+			} else {
+				newVal = 0;
+			}
+			target.val(newVal);
+		});
+	});
+	</script>
+	<?php }
 }
