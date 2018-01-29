440718d8f7509b3fa96b409ee8c253e128078338
KoalaZCooL <fazafauzanadhiman@gmail.com>
2018-01-30 10:03:16 +1100

    add favicon.ico

--- header.php
+++ header.php
@@ -12,6 +12,14 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
+<link rel="apple-touch-icon" sizes="180x180" href="<?=get_stylesheet_directory_uri()?>/icons/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="<?=get_stylesheet_directory_uri()?>/icons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="<?=get_stylesheet_directory_uri()?>/icons/favicon-16x16.png">
+<link rel="manifest" href="<?=get_stylesheet_directory_uri()?>/icons/manifest.json">
+<link rel="mask-icon" href="<?=get_stylesheet_directory_uri()?>/icons/safari-pinned-tab.svg" color="#ffffff">
+<link rel="shortcut icon" href="<?=get_stylesheet_directory_uri()?>/icons/favicon.ico">
+<meta name="msapplication-config" content="<?=get_stylesheet_directory_uri()?>/icons/browserconfig.xml">
+<meta name="theme-color" content="#4c4c4c">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
